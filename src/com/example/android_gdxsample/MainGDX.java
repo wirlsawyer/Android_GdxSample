305fcadcd1a95ab7ccb1212de6785e175d1db059
wirlsawyer <wirlsawyer@gmail.com>
2017-08-03 15:20:50 +0800

    [add] Touch Event

--- src/com/example/android_gdxsample/MainGDX.java
+++ src/com/example/android_gdxsample/MainGDX.java
@@ -1,27 +1,45 @@
 package com.example.android_gdxsample;
 
+import android.util.Log;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MainGDX implements ApplicationListener {
+	OrthographicCamera camera;
 	private SpriteBatch batch;
-	private Texture img;
-	
+	private Texture texture;
+	private Sprite sprite;
+
+
 	@Override
 	public void create() {
 		// TODO Auto-generated method stub
+		Gdx.input.setInputProcessor(onTouchEvent);
+		
+		int w = Gdx.graphics.getWidth();
+        int h = Gdx.graphics.getHeight();
+        camera = new OrthographicCamera(w, h);
+        camera.position.set(w/2, h/2, 0);
+        camera.update();
+        
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		texture = new Texture("badlogic.jpg");
+		sprite = new Sprite(texture);
+		sprite.setBounds( camera.position.x - (sprite.getWidth()/2), camera.position.y - (sprite.getHeight()/2), sprite.getWidth(), sprite.getHeight());
 	}
 
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
 		batch.dispose();
-		img.dispose();
+		texture.dispose();
 	}
 
 	
@@ -31,7 +49,8 @@ public class MainGDX implements ApplicationListener {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		sprite.draw(batch);
+		batch.draw(texture, 0, 0);
 		batch.end();
 	}
 
@@ -54,4 +73,17 @@ public class MainGDX implements ApplicationListener {
 		
 	}
 
+	private InputAdapter onTouchEvent = new InputAdapter(){
+		@Override
+        public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+
+            if(sprite.getBoundingRectangle().contains(screenX, screenY))
+            {
+            	Log.v("MainGDX", "Sprite Clicked");
+            }
+                 
+            return true;
+        }
+
+	};  
 }
